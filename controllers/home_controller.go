daed38972ad5f5ee33a7e00247f4a9a5ff43f778
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-02-05 09:39:18 +0700

    route to home is user name (#3)

--- controllers/home_controller.go
+++ controllers/home_controller.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/ockibagusp/golang-website-example/middleware"
+	"github.com/ockibagusp/golang-website-example/models"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -15,7 +17,7 @@ import (
  * @method: GET
  * @route: /
  */
-func (Controller) Home(c echo.Context) error {
+func (controller Controller) Home(c echo.Context) error {
 	// Please note the the second parameter "home.html" is the template name and should
 	// be equal to one of the keys in the TemplateRegistry array defined in main.go
 	session, _ := middleware.GetAuth(c)
@@ -24,12 +26,26 @@ func (Controller) Home(c echo.Context) error {
 		"route":    c.Path(),
 	})
 	log.Info("START request method GET for home")
-	// ---
+
+	var message string
+	if session.Values["username"] != "" {
+		var user models.User
+		if err := controller.DB.Select("name").Where(
+			"username = ?", session.Values["username"],
+		).First(&user); err.Error != nil {
+			log.Warnf(`session values "username" error: %v`, err.Error)
+		}
+
+		message = fmt.Sprintf("%v!", user.Name)
+		// or,
+		// session.Values["username"].(string) + "!"
+	}
+
 	log.Info("END request method GET for home: [+]success")
 	return c.Render(http.StatusOK, "home.html", echo.Map{
 		"name":    "Home",
 		"nav":     "home", // (?)
 		"session": session,
-		"msg":     "Ocki Bagus Pratama!",
+		"msg":     message,
 	})
 }
