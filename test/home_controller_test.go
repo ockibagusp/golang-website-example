322d867bcfd644247c92b228a7c8070484da73c8
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-02-07 17:59:30 +0700

    fix the test in the home controller a little (#3)

--- test/home_controller_test.go
+++ test/home_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/gavv/httpexpect/v2"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,68 +13,79 @@ func TestHomeController(t *testing.T) {
 	assert := assert.New(t)
 
 	no_auth := setupTestServer(t)
-	t.Run("home [no-auth] success", func(t *testing.T) {
-		result := no_auth.GET("/").
-			Expect().
-			Status(http.StatusOK)
-
-		no_auth_text := result.Body().Raw()
-
-		// TODO: why?
-
-		// navbar nav
-		regex := regexp.MustCompile(`<a href="/login" class="btn btn-outline-success my-2 my-sm-0">(.*)</a>`)
-		match := regex.FindString(no_auth_text)
-
-		actual := `<a href="/login" class="btn btn-outline-success my-2 my-sm-0">Login</a>`
-		assert.Equal(match, actual)
-
-		// main: jumbotron
-		regex = regexp.MustCompile(`<p class="lead">(.*)</p>`)
-		match = regex.FindString(no_auth_text)
-
-		assert.Equal(match, `<p class="lead">Test.</p>`)
-	})
-
 	auth_admin := setupTestServerAuth(no_auth, 1)
-	t.Run("home [admin] success", func(t *testing.T) {
-		result := auth_admin.GET("/").
-			Expect().
-			Status(http.StatusOK)
-
-		admin_text := result.Body().Raw()
-
-		// navbar nav
-		regex := regexp.MustCompile(`<a class="btn">(.*)</a>`)
-		match := regex.FindString(admin_text)
-
-		assert.Equal(match, `<a class="btn">ADMIN</a>`)
-
-		// main: jumbotron
-		regex = regexp.MustCompile(`<p class="lead">(.*)</p>`)
-		match = regex.FindString(admin_text)
-
-		assert.Equal(match, `<p class="lead">Admin.</p>`)
-	})
-
 	auth_user := setupTestServerAuth(no_auth, 0)
-	t.Run("home [user] success", func(t *testing.T) {
-		result := auth_user.GET("/").
-			Expect().
-			Status(http.StatusOK)
 
-		user_text := result.Body().Raw()
+	type regex struct {
+		must_compile string
+		actual       string
+	}
 
-		// navbar nav
-		regex := regexp.MustCompile(`<p class="lead">(.*)</p>`)
-		match := regex.FindString(user_text)
+	testCases := []struct {
+		name      string
+		expect    *httpexpect.Expect // auth_admin, auth_user or no-auth
+		navbar    regex
+		jumbotron regex
+	}{
+		{
+			name:   "home [no-auth] success",
+			expect: no_auth,
+			navbar: regex{
+				must_compile: `<a href="/login" class="btn btn-outline-success my-2 my-sm-0">(.*)</a>`,
+				actual:       `<a href="/login" class="btn btn-outline-success my-2 my-sm-0">Login</a>`,
+			},
+			jumbotron: regex{
+				must_compile: `<p class="lead">(.*)</p>`,
+				actual:       `<p class="lead">Test.</p>`,
+			},
+		},
+		{
+			name:   "home [admin] success",
+			expect: auth_admin,
+			navbar: regex{
+				must_compile: `<a class="btn">(.*)</a>`,
+				actual:       `<a class="btn">ADMIN</a>`,
+			},
+			jumbotron: regex{
+				must_compile: `<p class="lead">(.*)</p>`,
+				actual:       `<p class="lead">Admin.</p>`,
+			},
+		},
+		{
+			name:   "home [user] success",
+			expect: auth_user,
+			navbar: regex{},
+			jumbotron: regex{
+				must_compile: `<p class="lead">(.*)</p>`,
+				actual:       `<p class="lead">User.</p>`,
+			},
+		},
+	}
 
-		assert.Equal(match, `<p class="lead">User.</p>`)
+	for _, test := range testCases {
+		var result *httpexpect.Response
+		expect := test.expect // auth or no-auth
 
-		// main: jumbotron
-		regex = regexp.MustCompile(`<p class="lead">(.*)</p>`)
-		match = regex.FindString(user_text)
+		t.Run(test.name, func(t *testing.T) {
+			result = expect.GET("/").
+				Expect().
+				Status(http.StatusOK)
 
-		assert.Equal(match, `<p class="lead">User.</p>`)
-	})
+			result_body := result.Body().Raw()
+
+			// TODO: why?
+
+			// navbar nav
+			regex := regexp.MustCompile(test.navbar.must_compile)
+			match := regex.FindString(result_body)
+
+			assert.Equal(match, test.navbar.actual)
+
+			// main: jumbotron
+			regex = regexp.MustCompile(test.jumbotron.must_compile)
+			match = regex.FindString(result_body)
+
+			assert.Equal(match, test.jumbotron.actual)
+		})
+	}
 }
