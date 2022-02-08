428d11ec781f11c013116be28c5d3b182e53d8de
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-02-08 10:50:14 +0700

    add test in about controller (#3)

--- test/about_controller_test.go
+++ test/about_controller_test.go
@@ -2,22 +2,75 @@ package test
 
 import (
 	"net/http"
+	"regexp"
 	"testing"
+
+	"github.com/gavv/httpexpect/v2"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAboutSuccess(t *testing.T) {
-	noAuth := setupTestServer(t)
+	assert := assert.New(t)
 
-	t.Run("about success", func(t *testing.T) {
-		auth := setupTestServerAuth(noAuth, 0)
-		auth.GET("/about").
-			Expect().
-			Status(http.StatusOK)
-	})
+	no_auth := setupTestServer(t)
+	auth_admin := setupTestServerAuth(no_auth, 1)
+	auth_user := setupTestServerAuth(no_auth, 0)
 
-	t.Run("about failure", func(t *testing.T) {
-		noAuth.GET("/about").
-			Expect().
-			Status(http.StatusOK)
-	})
+	testCases := []struct {
+		name   string
+		expect *httpexpect.Expect // auth_admin, auth_user or no-auth
+		navbar regex
+		text   regex
+	}{
+		{
+			name:   "about [no-auth] success",
+			expect: no_auth,
+			navbar: regex{
+				must_compile: `<a href="/login" class="btn btn-outline-success my-2 my-sm-0">(.*)</a>`,
+				actual:       `<a href="/login" class="btn btn-outline-success my-2 my-sm-0">Login</a>`,
+			},
+			text: regex{},
+		},
+		{
+			name:   "about [admin] success",
+			expect: auth_admin,
+			navbar: regex{
+				must_compile: `<a class="btn">(.*)</a>`,
+				actual:       `<a class="btn">ADMIN</a>`,
+			},
+		},
+		{
+			name:   "home [user] success",
+			expect: auth_user,
+			navbar: regex{
+				must_compile: `<a href="/users" class="btn btn-outline-secondary my-2 my-sm-0">(.*)</a>`,
+				actual:       `<a href="/users" class="btn btn-outline-secondary my-2 my-sm-0">Users</a>`,
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		var result *httpexpect.Response
+		expect := test.expect // auth_admin, auth_user or no-auth
+
+		t.Run(test.name, func(t *testing.T) {
+			result = expect.GET("/about").
+				Expect().
+				Status(http.StatusOK)
+
+			result_body := result.Body().Raw()
+
+			// navbar nav
+			regex := regexp.MustCompile(test.navbar.must_compile)
+			match := regex.FindString(result_body)
+
+			assert.Equal(match, test.navbar.actual)
+
+			// main: text
+			regex = regexp.MustCompile(test.text.must_compile)
+			match = regex.FindString(result_body)
+
+			assert.Equal(match, test.text.actual)
+		})
+	}
 }
