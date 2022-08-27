ad9360e47c87a73cf703ea3479c3aca4fb4025f0
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-08-27 23:24:24 +0700

    [27/8/22] fix Echo server

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/ockibagusp/golang-website-example/controllers"
 	"github.com/ockibagusp/golang-website-example/router"
@@ -14,8 +18,21 @@ func main() {
 	// Echo: router
 	e := router.New(controllers)
 
-	// Start the Echo server
-	if err := e.Start(":8000"); err != nil {
-		log.Fatal(err)
+	// start the Echo server
+	go e.Logger.Fatal(e.Start(":8000"))
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	// defines a timeout context that will be canceled after 10 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// shutdown the Echo server
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(fmt.Sprintf("failed to shutting down Echo server: %v", err))
+	} else {
+		e.Logger.Info("successfully shutting down Echo server")
 	}
 }
