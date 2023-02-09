96d9b3ac6201dd471d3f8aa95f2ce6e40c3c8093
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2023-02-09 14:40:54 +0700

    [9/2/23] return delete router

--- app/main/router/router.go
+++ app/main/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/ockibagusp/golang-website-example/app/main/controller"
 	"github.com/ockibagusp/golang-website-example/app/main/template"
 	"github.com/ockibagusp/golang-website-example/config"
@@ -12,6 +13,10 @@ func RegisterPath(
 	appConfig *config.Config,
 	controller *controller.Controller,
 ) {
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	// Why bootstrap.min.css, bootstrap.min.js, jquery.min.js?
 	e.Static("/assets", "assets")
 
