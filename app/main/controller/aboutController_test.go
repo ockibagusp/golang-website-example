b5bbbadaf38bd656ddd598961953ec662f976c10
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2023-03-10 19:52:53 +0700

    [10/3/23] add testing aboutControoler_test.go

---
+++ app/main/controller/aboutController_test.go
@@ -0,0 +1,73 @@
+package controller_test
+
+import (
+	"net/http"
+	"regexp"
+	"testing"
+
+	"github.com/gavv/httpexpect/v2"
+	methodTest "github.com/ockibagusp/golang-website-example/app/main/controller/mock/method"
+	modelsTest "github.com/ockibagusp/golang-website-example/app/main/controller/mock/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAboutSuccess(t *testing.T) {
+	assert := assert.New(t)
+
+	no_auth := setupTestServer(t)
+
+	// test for SetSession = false
+	methodTest.SetSession = false
+	// test for db users
+	truncateUsers()
+
+	test_cases := []struct {
+		name        string
+		expect      string // admin, sugriwa
+		html_navbar regex
+	}{
+		{
+			name:   "about [no-auth] success",
+			expect: ANONYMOUS,
+			html_navbar: regex{
+				must_compile: `<a href="/login" class="btn btn-outline-success my-2 my-sm-0">(.*)</a>`,
+				actual:       `<a href="/login" class="btn btn-outline-success my-2 my-sm-0">Login</a>`,
+			},
+		},
+		{
+			name:   "about [admin] success",
+			expect: ADMIN,
+			html_navbar: regex{
+				must_compile: `<a class="btn">(.*)</a>`,
+				actual:       `<a class="btn">ADMIN</a>`,
+			},
+		},
+		{
+			name:   "home [user] success",
+			expect: SUGRIWA,
+			html_navbar: regex{
+				must_compile: `<a href="/users" class="btn btn-outline-secondary my-2 my-sm-0">(.*)</a>`,
+				actual:       `<a href="/users" class="btn btn-outline-secondary my-2 my-sm-0">Users</a>`,
+			},
+		},
+	}
+
+	for _, test := range test_cases {
+		var result *httpexpect.Response
+		modelsTest.UserSelectTest = test.expect // auth_admin, auth_sugriwa or no-auth
+
+		t.Run(test.name, func(t *testing.T) {
+			result = no_auth.GET("/about").
+				Expect().
+				Status(http.StatusOK)
+
+			result_body := result.Body().Raw()
+
+			// navbar nav
+			regex := regexp.MustCompile(test.html_navbar.must_compile)
+			match := regex.FindString(result_body)
+
+			assert.Equal(match, test.html_navbar.actual)
+		})
+	}
+}
