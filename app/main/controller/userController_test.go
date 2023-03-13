8beb780744eba73a27be6a31f843943c0c8bf075
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2023-03-13 13:34:53 +0700

    [13/3/23] add controller CreateUser: false

--- app/main/controller/userController_test.go
+++ app/main/controller/userController_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gavv/httpexpect/v2"
 	"github.com/ockibagusp/golang-website-example/app/main/controller/mock/method"
 	modelsTest "github.com/ockibagusp/golang-website-example/app/main/controller/mock/models"
+	"github.com/ockibagusp/golang-website-example/app/main/types"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,8 +21,6 @@ func TestUsersController(t *testing.T) {
 	method.SetSession = false
 	// test for db users
 	truncateUsers()
-
-	// TODO: rows all, admin dan user, insyaallah
 
 	test_cases := []struct {
 		name         string
@@ -202,6 +201,273 @@ func TestUsersController(t *testing.T) {
 
 				regex = regexp.MustCompile(must_compile)
 				match = regex.FindString(result_body)
+
+				assert.Equal(match, actual)
+			}
+
+			statusCode := result.Raw().StatusCode
+			if test.status != statusCode {
+				t.Logf(
+					"got: %d but expect %d", test.status, statusCode,
+				)
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestCreateUserController(t *testing.T) {
+	no_auth := setupTestServer(t)
+
+	// test for SetSession = false
+	method.SetSession = false
+	// test for db users
+	truncateUsers()
+
+	// TODO: flash with redirect on failure, insyaallah
+
+	test_cases := []struct {
+		name   string
+		method int    // method: 1=GET or 2=POST
+		expect string // auth or no-auth
+		form   types.UserForm
+		status int
+
+		// body navbar
+		html_navbar regex
+		// body heading
+		html_heading regex
+		// flash message
+		html_flash_success regex
+		html_flash_error   regex
+	}{
+		/*
+			create it [admin]
+		*/
+		// GET
+		{
+			name:   "users [admin] to GET create it success",
+			expect: ADMIN,
+			method: method.HTTP_REQUEST_GET,
+			// HTTP response status: 200 OK
+			status: http.StatusOK,
+			// body navbar
+			html_navbar: regex{
+				must_compile: `<a class="btn">(.*)</a>`,
+				actual:       `<a class="btn">ADMIN</a>`,
+			},
+			// body heading
+			html_heading: regex{
+				must_compile: `<h2 class="mt-4">(.*)</h2>`,
+				actual:       `<h2 class="mt-4">New User</h2>`,
+			},
+		},
+		// POST
+		{
+			name:   "user [admin] to POST create it success",
+			expect: ADMIN,
+			method: method.HTTP_REQUEST_POST,
+			form: types.UserForm{
+				Username:        "unit-test",
+				Email:           "unit-test@exemple.com",
+				Name:            "Unit Test",
+				Password:        "unit-test",
+				ConfirmPassword: "unit-test",
+			},
+			// HTTP response status: 200 OK
+			status: http.StatusOK,
+			// body navbar
+			html_navbar: regex{
+				must_compile: `<a class="btn">(.*)</a>`,
+				actual:       `<a class="btn">ADMIN</a>`,
+			},
+			// body heading
+			html_heading: regex{
+				must_compile: `<h2 class="mt-4">(.*)</h2>`,
+				actual:       `<h2 class="mt-4">Users: All</h2>`,
+			},
+			// flash message success
+			html_flash_success: regex{
+				must_compile: `<strong>success:</strong> (.*)`,
+				actual:       `<strong>success:</strong> success new user: unit-test!`,
+			},
+		},
+		// Database: " Error 1062: Duplicate entry 'unit-test@exemple.com' for key 'email_UNIQUE' " v
+		//			-> " Error 1062: Duplicate entry 'unit-test@exemple.com' for key 'users.email_UNIQUE' " x
+		{
+			name:   "users [admin] to POST create it failure: Duplicate entry",
+			expect: ADMIN,
+			method: method.HTTP_REQUEST_POST,
+			form: types.UserForm{
+				Username:        "unit-test",
+				Email:           "unit-test@exemple.com",
+				Name:            "Unit Test",
+				Password:        "unit-test",
+				ConfirmPassword: "unit-test",
+			},
+			// HTTP response status: 400 Bad Request
+			status: http.StatusBadRequest,
+			// body navbar
+			html_navbar: regex{
+				must_compile: `<a class="btn">(.*)</a>`,
+				actual:       `<a class="btn">ADMIN</a>`,
+			},
+			// body heading
+			html_heading: regex{
+				must_compile: `<h2 class="mt-4">(.*)</h2>`,
+				actual:       `<h2 class="mt-4">New User</h2>`,
+			},
+			// flash message error
+			html_flash_error: regex{
+				must_compile: `<strong>error:</strong> (.*)`,
+				actual:       `<strong>error:</strong> Error 1062: Duplicate entry &#39;unit-test@exemple.com&#39; for key &#39;email_UNIQUE&#39;!`,
+			},
+		},
+
+		/*
+			create it [sugriwa]
+		*/
+		// GET
+		{
+			name:   "users [sugriwa] to GET create it failure",
+			expect: SUGRIWA,
+			method: method.HTTP_REQUEST_GET,
+			// redirect @route: /users/read/:id
+			// HTTP response status: 200 OK
+			status: http.StatusOK,
+			// body heading
+			html_heading: regex{
+				must_compile: `<h2 class="mt-4">(.*)</h2>`,
+				actual:       `<h2 class="mt-4">User: Sugriwa</h2>`,
+			},
+		},
+		// POST
+		{
+			name:   "user [sugriwa] to POST create it failure",
+			expect: SUGRIWA,
+			method: method.HTTP_REQUEST_POST,
+			// redirect @route: /users/read/:id
+			// HTTP response status: 200 OK
+			status: http.StatusOK,
+			// body heading
+			html_heading: regex{
+				must_compile: `<h2 class="mt-4">(.*)</h2>`,
+				actual:       `<h2 class="mt-4">User: Sugriwa</h2>`,
+			},
+			// flash message error
+			html_flash_error: regex{
+				must_compile: `<strong>error:</strong> (.*)`,
+				actual:       `<strong>error:</strong> 403 Forbidden!`,
+			},
+		},
+
+		/*
+			create it [no-auth]
+		*/
+		// GET
+		{
+			name:   "users [no-auth] to GET create it success",
+			expect: "anonymous",
+			method: method.HTTP_REQUEST_GET,
+			// HTTP response status: 200 OK
+			status: http.StatusOK,
+			// body navbar
+			html_navbar: regex{
+				must_compile: `<a href="/login" (.*)>Login</a>`,
+				actual:       `<a href="/login" class="btn btn-outline-success my-2 my-sm-0">Login</a>`,
+			},
+			// body heading
+			html_heading: regex{
+				must_compile: `<h2 class="mt-4">(.*)</h2>`,
+				actual:       `<h2 class="mt-4">New User</h2>`,
+			},
+		},
+		// POST
+		{
+			name:   "user [no-auth] to POST create it success",
+			expect: "anonymous",
+			method: method.HTTP_REQUEST_POST,
+			form: types.UserForm{
+				Username:        "example",
+				Email:           "example@example.com",
+				Name:            "Example",
+				Password:        "example123",
+				ConfirmPassword: "example123",
+			},
+			// HTTP response status: 200 OK
+			status: http.StatusOK,
+			// flash message success
+			html_flash_success: regex{
+				must_compile: `<strong>success:</strong> (.*)`,
+				actual:       `<strong>success:</strong> success new user: example!`,
+			},
+			// TODO: difficult html_navbar and html_heading, insyaallah
+		},
+	}
+
+	for _, test := range test_cases {
+		modelsTest.UserSelectTest = test.expect
+
+		t.Run(test.name, func(t *testing.T) {
+			var result *httpexpect.Response
+			if test.method == method.HTTP_REQUEST_GET {
+				result = no_auth.GET("/users/add").
+					WithForm(test.form).
+					Expect().
+					Status(test.status)
+			} else if test.method == method.HTTP_REQUEST_POST {
+				result = no_auth.POST("/users/add").
+					WithForm(test.form).
+					Expect().
+					Status(test.status)
+			} else {
+				panic("method: 1=GET or 2=POST")
+			}
+
+			result_body := result.Body().Raw()
+
+			var (
+				must_compile, actual, match string
+				regex                       *regexp.Regexp
+			)
+
+			if test.html_flash_success.must_compile != "" {
+				must_compile = test.html_flash_success.must_compile
+				actual = test.html_flash_success.actual
+
+				regex = regexp.MustCompile(must_compile)
+				match = regex.FindString(result_body)
+
+				assert.Equal(t, match, actual)
+			}
+
+			if test.html_flash_error.must_compile != "" {
+				must_compile = test.html_flash_error.must_compile
+				actual = test.html_flash_error.actual
+
+				regex = regexp.MustCompile(must_compile)
+				match = regex.FindString(result_body)
+
+				assert.Equal(t, match, actual)
+			}
+
+			assert := assert.New(t)
+			if test.html_navbar.must_compile != "" {
+				must_compile = test.html_navbar.must_compile
+				actual = test.html_navbar.actual
+
+				regex = regexp.MustCompile(must_compile)
+				match = regex.FindString(result_body)
+
+				assert.Equal(match, actual)
+			}
+
+			if test.html_heading.must_compile != "" {
+				must_compile = test.html_heading.must_compile
+				actual = test.html_heading.actual
+
+				regex := regexp.MustCompile(must_compile)
+				match := regex.FindString(result_body)
 
 				assert.Equal(match, actual)
 			}
