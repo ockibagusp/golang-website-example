ac2e2c17202a951d683d178344a9757aa16f8b0b
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2023-04-08 13:13:48 +0700

    [8/4/23] add testing controller user DeletePermanentlyByID

--- app/main/controller/adminController_test.go
+++ app/main/controller/adminController_test.go
@@ -340,3 +340,190 @@ func TestAdminRestoreByID(t *testing.T) {
 		})
 	}
 }
+
+func TestDeletePermanentlyByID(t *testing.T) {
+	noAuth := setupTestServer(t)
+
+	// test for SetSession = false
+	methodTest.SetSession = false
+	// test for db users
+	truncateUsers()
+
+	// delete user subali (id=3)
+	err := newUserService(db).Delete(business.InternalContext{}, 3)
+	if err != nil {
+		panic("subali: username not already: " + err.Error())
+	}
+
+	testCases := []struct {
+		name             string
+		expect           string // auth or no-auth
+		path             string
+		status           int
+		htmlNavbar       regex
+		htmlHeading      regex
+		htmlFlashSuccess regex
+		jsonMessageError regex
+	}{
+		/*
+			delete permanently by id [admin]
+		*/
+		{
+			name:   "delete permanently by id [admin] to GET it failure: id=1",
+			expect: ADMIN,
+			path:   "1",
+			// HTTP response status: 403 Forbidden
+			status: http.StatusForbidden,
+			jsonMessageError: regex{
+				mustCompile: `{"message":"(.*)"}`,
+				actual:      `{"message":"Forbidden"}`,
+			},
+		},
+		{
+			name:   "delete permanently by id [admin] to GET it success: id=2",
+			expect: ADMIN,
+			path:   "3",
+			// HTTP response status: 200 OK
+			status: http.StatusOK,
+			// body navbar
+			htmlNavbar: regex{
+				mustCompile: `<a class="btn">(.*)</a>`,
+				actual:      `<a class="btn">ADMIN</a>`,
+			},
+			// body heading
+			htmlHeading: regex{
+				mustCompile: `<h2 class="mt-4">(.*)</h2>`,
+				actual:      `<h2 class="mt-4">Delete Permanently?</h2>`,
+			},
+			// flash message success
+			htmlFlashSuccess: regex{
+				mustCompile: `<strong>success:</strong> (.*)`,
+				actual:      `<strong>success:</strong> success permanently user: subali!`,
+			},
+		},
+		{
+			name:   "delete permanently by id [admin] to GET it failure: id=-1",
+			expect: ADMIN,
+			path:   "-1",
+			// HTTP response status: 404 Not Found
+			status: http.StatusNotFound,
+			jsonMessageError: regex{
+				mustCompile: `{"message":"(.*)"}`,
+				actual:      `{"message":"User Not Found"}`,
+			},
+		},
+
+		/*
+			delete permanently by id [sugriwa]
+		*/
+		{
+			name:   "delete permanently by id [sugriwa] to GET it success: id=1",
+			expect: SUGRIWA,
+			path:   "1",
+			// HTTP response status: 404 Not Found
+			status: http.StatusNotFound,
+			jsonMessageError: regex{
+				mustCompile: `{"message":"(.*)"}`,
+				actual:      `{"message":"Not Found"}`,
+			},
+		},
+
+		/*
+			No Auth
+		*/
+		{
+			name:   "delete permanently by id [no-auth] to GET it failure: id=1",
+			expect: ANONYMOUS,
+			// HTTP response status: 404 Not Found
+			status: http.StatusNotFound,
+			jsonMessageError: regex{
+				mustCompile: `{"message":"(.*)"}`,
+				actual:      `{"message":"Not Found"}`,
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		var result *httpexpect.Response
+		/*
+			expect := test.expect
+
+			or,
+
+			var expect = test.expect
+		*/
+		modelsTest.UserSelectTest = test.expect // ADMIN and SUGRIWA
+
+		t.Run(test.name, func(t *testing.T) {
+			// @route: exemple "/admin/delete/permanently/3"
+			result = noAuth.GET("/admin/delete/permanently/{id}", test.path).
+				Expect().
+				Status(test.status)
+
+			resultBody := result.Body().Raw()
+
+			var (
+				mustCompile, actual, match string
+				regex                      *regexp.Regexp
+			)
+
+			if test.htmlNavbar.mustCompile != "" {
+				mustCompile = test.htmlNavbar.mustCompile
+				actual = test.htmlNavbar.actual
+
+				regex = regexp.MustCompile(mustCompile)
+				match = regex.FindString(resultBody)
+
+				// assert.Equal(t, match, actual)
+				//
+				// or,
+				//
+				// assert := assert.New(t)
+				// ...
+				// assert.Equal(match, actual)
+				assert.Equal(t, match, actual)
+			}
+
+			if test.htmlHeading.mustCompile != "" {
+				mustCompile = test.htmlHeading.mustCompile
+				actual = test.htmlHeading.actual
+
+				regex = regexp.MustCompile(mustCompile)
+				match = regex.FindString(resultBody)
+
+				assert.Equal(t, match, actual)
+			}
+
+			if test.htmlFlashSuccess.mustCompile != "" {
+				mustCompile = test.htmlFlashSuccess.mustCompile
+				actual = test.htmlFlashSuccess.actual
+
+				regex = regexp.MustCompile(mustCompile)
+				match = regex.FindString(resultBody)
+
+				assert.Equal(t, match, actual)
+			}
+
+			if test.jsonMessageError.mustCompile != "" {
+				mustCompile = test.jsonMessageError.mustCompile
+				actual = test.jsonMessageError.actual
+
+				regex = regexp.MustCompile(mustCompile)
+				match = regex.FindString(resultBody)
+
+				assert.Equal(t, match, actual)
+			}
+
+			statusCode := result.Raw().StatusCode
+			if test.status != statusCode {
+				t.Logf(
+					"got: %d but expect %d", test.status, statusCode,
+				)
+				t.Fail()
+			}
+		})
+	}
+
+	// test for db users
+	truncateUsers()
+}
